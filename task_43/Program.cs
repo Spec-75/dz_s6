e18f43133e8481f8bbf8cec31cb03111e95596f4
Spec-75 <Spec_75@mail.ru>
2022-07-15 11:52:37 +0300

    add task_43

--- task_43/Program.cs
+++ task_43/Program.cs
@@ -11,8 +11,13 @@ Console.WriteLine("Enter b2");
 double b2 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Enter k2");
 double k2 = Convert.ToDouble(Console.ReadLine());
-double x = (b2-b1)/(k1-k2);
-double y = k1*x+b1;
-Console.WriteLine($"the x coordinate of the intersection point of the lines {x}");
-Console.WriteLine($"the y coordinate of the intersection point of the lines {y}");
+if (k1 != k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"the x coordinate of the intersection point of the lines {x}");
+    Console.WriteLine($"the y coordinate of the intersection point of the lines {y}");
+}
+else Console.WriteLine("straight lines don't intersect");
+
 
