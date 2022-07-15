d70d12b58e9a34e4d53fb6cf5a26cf47e5b0f160
Spec-75 <Spec_75@mail.ru>
2022-07-15 09:42:41 +0300

    add task_41

--- task_41/Program.cs
+++ task_41/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+//сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Enter the required number of numbers");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[number];
+int countPositivNumber = 0;
+for (int i = 0; i < number; i++)
+{
+    Console.WriteLine("Enter the next number");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if (array[i] > 0) countPositivNumber++;
+}
+Console.WriteLine($"number of positive numbers: {countPositivNumber}");
